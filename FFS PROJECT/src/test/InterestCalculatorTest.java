969683f3a1b6c576d12e2647daaf2df160664b99
Tang-martin <33925355+Tang-martin@users.noreply.github.com>
2018-05-16 10:56:19 +0200

    calculate alogithm made
    added BadCreditRating exception class
    added InterestCalculator class with calculation alogrith
    3 test cases added to InteresCalculatorTest

---
+++ FFS PROJECT/src/test/InterestCalculatorTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.ferrari.finances.dk.rki.Rating;
+
+import logic.BadCreditRating;
+import logic.InterestCalculator;
+
+public class InterestCalculatorTest {
+
+	@Test
+	public void calcInterestRateTest() throws BadCreditRating {
+		InterestCalculator intcalc = new InterestCalculator();
+		double currentRate = 5.0;
+		Rating rate = Rating.A;
+		int downPayment = 250000;
+		int numberOfMonts = 60;
+		int carPrice = 1000000;
+		
+		assertEquals(8.0,intcalc.calcInterestRate(rate, currentRate, downPayment, numberOfMonts, carPrice),0);
+	}
+	
+	@Test
+	public void calcInterestRateTest2() throws BadCreditRating {
+		InterestCalculator intcalc = new InterestCalculator();
+		double currentRate = 5.0;
+		Rating rate = Rating.B;
+		int downPayment = 250000;
+		int numberOfMonts = 60;
+		int carPrice = 1000000;
+		
+		assertEquals(9.0,intcalc.calcInterestRate(rate, currentRate, downPayment, numberOfMonts, carPrice),0);
+	}
+	
+	@Test
+	public void calcInterestRateTest3() throws BadCreditRating {
+		InterestCalculator intcalc = new InterestCalculator();
+		double currentRate = 5.0;
+		Rating rate = Rating.C;
+		int downPayment = 250000;
+		int numberOfMonts = 60;
+		int carPrice = 1000000;
+		
+		assertEquals(10.0,intcalc.calcInterestRate(rate, currentRate, downPayment, numberOfMonts, carPrice),0);
+	}
+
+}
