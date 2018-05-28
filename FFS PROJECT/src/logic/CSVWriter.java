2f7c19aaac770a52441a687f9d9601b1ac71778c
Tang-martin <33925355+Tang-martin@users.noreply.github.com>
2018-05-28 12:50:47 +0200

    refactoring

--- FFS PROJECT/src/logic/CSVWriter.java
+++ FFS PROJECT/src/logic/CSVWriter.java
@@ -6,20 +6,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CSVWriter {
-	private String sep = ";";
+	private String separator = ";";
+	Calculator calculator = new Calculator();
 
 	public void exportLoan(LoanOffer loanOffer) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter("Låne Tilbud " + loanOffer.getId() + ".csv"));
 		
-		HashMap<LoanPlanComponent, ArrayList<Double>> comps = Calculator.loanPlanCalculation(loanOffer);
-		writer.write("Termin" + sep + "Fast Ydelse" + sep + "Rente pr. termin" + sep + "Afdrag" + sep + "Ultimo restgæld");
+		HashMap<LoanPlanComponent, ArrayList<Double>> comps = calculator.loanPlanCalculation(loanOffer);
+		writer.write("Termin" + separator + "Fast Ydelse" + separator + "Rente pr. termin" + separator + "Afdrag" + separator + "Ultimo restgæld");
 		writer.newLine();
 		
 		for (int i = 0; i < loanOffer.getNumberOfMonths(); i++) {
-			writer.write(i + sep + loanOffer.getRepayments() + sep + comps.get(LoanPlanComponent.RATE).get(i) + sep + 
-					comps.get(LoanPlanComponent.INSTALL).get(i) + sep + comps.get(LoanPlanComponent.OUT_DEBT).get(i));
+			writer.write(i + separator + loanOffer.getRepayments() + separator + comps.get(LoanPlanComponent.RATE).get(i) + separator + 
+					comps.get(LoanPlanComponent.INSTALL).get(i) + separator + comps.get(LoanPlanComponent.OUT_DEBT).get(i));
 			writer.newLine();
 		}
+		
+		writer.close();
 		
 	}
 }
